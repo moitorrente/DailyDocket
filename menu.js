2d446f91880d614ef944fe8c6f9ffdae1e1380cd
moitorrente <moi.torrente@gmail.com>
2023-02-23 00:57:38 +0100

    Añadida la opción de eliminar una tarea desde menú

--- menu.js
+++ menu.js
@@ -1,58 +1,76 @@
 class CustomMenu extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.lastTaskClicked = null;
+    this.shadowRoot.innerHTML = `
+      <style>
+        #custom-menu {
+          display: none;
+          position: absolute;
+          background-color: #f9fafb;
+          border: 1px solid #d1d5db;
+          font-size: small;
+          padding: 2px;
+          border-radius: 10px;
+          box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
+          z-index: 1;
+        }
 
-        this.shadowRoot.innerHTML = `
-        <style>
-          #custom-menu {
-            display: none;
-            position: absolute;
-            background-color: #f1f1f1;
-            border: 1px solid #d3d3d3;
-            z-index: 1;
-          }
-  
-          #custom-menu a {
-            color: black;
-            padding: 8px 16px;
-            text-decoration: none;
-            display: block;
-          }
-  
-          #custom-menu a:hover {
-            background-color: #d3d3d3;
-          }
-        </style>
-  
-        <div id="custom-menu">
-          <a href="#">Opción 1</a>
-          <a href="#">Opción 2</a>
-          <a href="#">Opción 3</a>
-        </div>
-      `;
+        #custom-menu a {
+          color: black;
+          padding: 6px 14px;
+          text-decoration: none;
+          display: block;
+        }
 
-        this.menu = this.shadowRoot.getElementById('custom-menu');
+        #custom-menu a:hover {
+          background-color: #e5e7eb;
+          border-radius: 8px;
+        }
+      </style>
 
-        document.addEventListener('contextmenu', this.showMenu.bind(this));
-        document.addEventListener('click', this.hideMenu.bind(this));
-    }
+      <div id="custom-menu">
+        <a href="#">Eliminar</a>
+      </div>
+    `;
 
-    showMenu(event) {
+    this.menu = this.shadowRoot.getElementById('custom-menu');
+
+    // Agregamos un manejador de eventos al hacer clic en los enlaces
+    this.shadowRoot.querySelectorAll('#custom-menu a').forEach((link, index) => {
+      link.addEventListener('click', (event) => {
         event.preventDefault();
-        const x = event.clientX;
-        const y = event.clientY;
-        const targetElement = document.elementFromPoint(x, y);
-        const closestCustomMenu = targetElement.closest('task-list');
+        if (index === 0) {
+          removeOpenTask(Number(this.lastTaskClicked));
+          removeClosedTask(Number(this.lastTaskClicked));
+        }
+        this.hideMenu();
+      });
+    });
 
-        this.menu.style.left = `${event.pageX}px`;
-        this.menu.style.top = `${event.pageY}px`;
-        this.menu.style.display = 'block';
-    }
+    document.addEventListener('contextmenu', this.showMenu.bind(this));
+    document.addEventListener('click', this.hideMenu.bind(this));
+  }
 
-    hideMenu() {
-        this.menu.style.display = 'none';
+  showMenu(event) {
+
+    const x = event.clientX;
+    const y = event.clientY;
+    const targetElement = document.elementFromPoint(x, y);
+    const closestTaskList = targetElement.closest('task-list');
+    if (closestTaskList) {
+      event.preventDefault();
+      this.lastTaskClicked = closestTaskList.id
+      this.menu.style.left = `${event.pageX}px`;
+      this.menu.style.top = `${event.pageY}px`;
+      this.menu.style.display = 'block';
     }
+  }
+
+  hideMenu() {
+    this.menu.style.display = 'none';
+  }
 }
 
 customElements.define('custom-menu', CustomMenu);
