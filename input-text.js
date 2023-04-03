ee5af6895ac9c4983d0cc7f218cfa3eec3a85f44
moitorrente <moi.torrente@gmail.com>
2023-04-03 19:31:46 +0200

    Cambio icono

--- input-text.js
+++ input-text.js
@@ -64,7 +64,7 @@ class InputText extends HTMLElement {
         input.style.backgroundColor = '#e5e7eb';
         input.style.color = '#4b5563';
         input.style.borderRadius = '5px';
-        input.style.padding = '12px';
+        input.style.padding = '10px';
         input.style.fontWeight = '400';
         input.style.fontSize = '18px';
 
@@ -295,17 +295,7 @@ class InputText extends HTMLElement {
                             hotKeys.style.gap = '3px';
 
                             const alt = document.createElement('div');
-                            alt.innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M4.75 6.75V17.25C4.75 18.3546 5.64543 19.25 6.75 19.25H17.25C18.3546 19.25 19.25 18.3546 19.25 17.25V6.75C19.25 5.64543 18.3546 4.75 17.25 4.75H6.75C5.64543 4.75 4.75 5.64543 4.75 6.75Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                            <path d="M8.5 8C8.5 8.27614 8.27614 8.5 8 8.5C7.72386 8.5 7.5 8.27614 7.5 8C7.5 7.72386 7.72386 7.5 8 7.5C8.27614 7.5 8.5 7.72386 8.5 8Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-                            <path d="M8.5 12C8.5 12.2761 8.27614 12.5 8 12.5C7.72386 12.5 7.5 12.2761 7.5 12C7.5 11.7239 7.72386 11.5 8 11.5C8.27614 11.5 8.5 11.7239 8.5 12Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-                            <path d="M12.5 8C12.5 8.27614 12.2761 8.5 12 8.5C11.7239 8.5 11.5 8.27614 11.5 8C11.5 7.72386 11.7239 7.5 12 7.5C12.2761 7.5 12.5 7.72386 12.5 8Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-                            <path d="M12.5 12C12.5 12.2761 12.2761 12.5 12 12.5C11.7239 12.5 11.5 12.2761 11.5 12C11.5 11.7239 11.7239 11.5 12 11.5C12.2761 11.5 12.5 11.7239 12.5 12Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-                            <path d="M16.5 8C16.5 8.27614 16.2761 8.5 16 8.5C15.7239 8.5 15.5 8.27614 15.5 8C15.5 7.72386 15.7239 7.5 16 7.5C16.2761 7.5 16.5 7.72386 16.5 8Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-                            <path d="M16.5 12C16.5 12.2761 16.2761 12.5 16 12.5C15.7239 12.5 15.5 12.2761 15.5 12C15.5 11.7239 15.7239 11.5 16 11.5C16.2761 11.5 16.5 11.7239 16.5 12Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-                            <path d="M7.75 16.25H16.25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                            </svg>
-                            `;
+                            alt.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-command"><path d="M18 3a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3H6a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3V6a3 3 0 0 0-3-3 3 3 0 0 0-3 3 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 3 3 0 0 0-3-3z"></path></svg>`;
                             alt.style.color = '#9ca3af';
                             alt.style.fontSize = '10px';
                             alt.style.backgroundColor = '#6b7280';
@@ -404,7 +394,7 @@ class InputText extends HTMLElement {
             commandContainer.style.color = '#f9fafb';
         }
         commandContainer.style.borderRadius = '5px';
-        commandContainer.style.padding = '12px';
+        commandContainer.style.padding = '8px';
         commandContainer.style.fontWeight = '500';
         commandContainer.style.fontSize = '18px';
         mainContainer.appendChild(commandContainer);
